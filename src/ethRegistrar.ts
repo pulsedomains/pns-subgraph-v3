fb350836898058011dc9a178a0785d4108be70d2
Nick Johnson <arachnid@notdot.net>
2019-06-07 17:16:49 +1200

    Added missing files

---
+++ src/ethRegistrar.ts
@@ -0,0 +1,121 @@
+// Import types and APIs from graph-ts
+import {
+  BigInt,
+  ByteArray,
+  crypto,
+  ens,
+  EthereumEvent,
+} from '@graphprotocol/graph-ts'
+
+// Import event types from the registry contract ABI
+import {
+  NameMigrated as NameMigratedEvent,
+  NameRegistered as NameRegisteredEvent,
+  NameRenewed as NameRenewedEvent
+} from './types/BaseRegistrar/BaseRegistrar'
+
+import {
+  NameRegistered as ControllerNameRegisteredEvent
+} from './types/EthRegistrarController/EthRegistrarController'
+
+// Import entity types generated from the GraphQL schema
+import { Account, AuctionedName, Domain, Registration, NameMigrated, NameRegistered, NameRenewed } from './types/schema'
+
+var rootNode:ByteArray = byteArrayFromHex("93cdeb708b7545dc668eb9280176169d1c33cfd8ed6f04690a0bcc88a93fc4ae")
+
+export function handleNameMigrated(event: NameMigratedEvent): void {
+  let label = uint256ToByteArray(event.params.id)
+
+  let auctionedName = AuctionedName.load(label.toHex())
+
+  let registration = new Registration(label.toHex())
+  registration.domain = crypto.keccak256(concat(rootNode, label)).toHex();
+  registration.registrationDate = auctionedName.registrationDate
+  registration.expiryDate = event.params.expires
+  registration.registrant = event.params.owner.toHex()
+  registration.save()
+
+  let registrationEvent = new NameMigrated(createEventID(event))
+  registrationEvent.registration = registration.id
+  registrationEvent.blockNumber = event.block.number.toI32()
+  registrationEvent.transactionID = event.transaction.hash
+  registrationEvent.registrant = event.params.owner.toHex()
+  registrationEvent.expiryDate = event.params.expires
+  registrationEvent.save()
+}
+
+export function handleNameRegistered(event: NameRegisteredEvent): void {
+  let account = new Account(event.params.owner.toHex())
+  account.save()
+
+  let label = uint256ToByteArray(event.params.id)
+  let registration = new Registration(label.toHex())
+  registration.domain = crypto.keccak256(concat(rootNode, label)).toHex()
+  registration.registrationDate = event.block.timestamp
+  registration.expiryDate = event.params.expires
+  registration.registrant = account.id
+  registration.save()
+
+  let registrationEvent = new NameRegistered(createEventID(event))
+  registrationEvent.registration = registration.id
+  registrationEvent.blockNumber = event.block.number.toI32()
+  registrationEvent.transactionID = event.transaction.hash
+  registrationEvent.registrant = account.id
+  registrationEvent.expiryDate = event.params.expires
+  registrationEvent.save()
+}
+
+export function handleNameRegisteredByController(event: ControllerNameRegisteredEvent): void {
+  let domain = new Domain(crypto.keccak256(concat(rootNode, event.params.label)).toHex())
+  if(domain.labelName !== event.params.name) {
+    domain.labelName = event.params.name
+    domain.name = event.params.name + '.eth'
+    domain.save()
+  }
+}
+
+export function handleNameRenewed(event: NameRenewedEvent): void {
+  let label = uint256ToByteArray(event.params.id)
+  let registration = new Registration(label.toHex())
+  registration.expiryDate = event.params.expires
+  registration.save()
+
+  let registrationEvent = new NameRenewed(createEventID(event))
+  registrationEvent.registration = registration.id
+  registrationEvent.blockNumber = event.block.number.toI32()
+  registrationEvent.transactionID = event.transaction.hash
+  registrationEvent.expiryDate = event.params.expires
+  registrationEvent.save()
+}
+
+// Helper for concatenating two byte arrays
+function concat(a: ByteArray, b: ByteArray): ByteArray {
+  let out = new Uint8Array(a.length + b.length)
+  for (let i = 0; i < a.length; i++) {
+    out[i] = a[i]
+  }
+  for (let j = 0; j < b.length; j++) {
+    out[a.length + j] = b[j]
+  }
+  return out as ByteArray
+}
+
+function byteArrayFromHex(s: string): ByteArray {
+  if(s.length % 2 !== 0) {
+    throw new TypeError("Hex string must have an even number of characters")
+  }
+  let out = new Uint8Array(s.length / 2)
+  for(var i = 0; i < s.length; i += 2) {
+    out[i / 2] = parseInt(s.substring(i, i + 2), 16) as u32
+  }
+  return out as ByteArray;
+}
+
+function uint256ToByteArray(i: BigInt): ByteArray {
+  let hex = i.toHex().slice(2).padStart(64, '0')
+  return byteArrayFromHex(hex)
+}
+
+function createEventID(event: EthereumEvent): string {
+  return event.block.number.toString().concat('-').concat(event.logIndex.toString())
+}

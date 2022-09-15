85c7608476585281498572e49b109b51ebe8bc0c
Makoto Inoue <2630+makoto@users.noreply.github.com>
2022-09-15 13:28:17 +0100

    Add ReverseClaimed (#53)
    * Add ReverseClaimed
    
    * Rename primaryName to name

--- src/resolver.ts
+++ src/resolver.ts
@@ -70,6 +70,10 @@ export function handleMulticoinAddrChanged(event: AddressChangedEvent): void {
 export function handleNameChanged(event: NameChangedEvent): void {
   if(event.params.name.indexOf("\u0000") != -1) return;
   
+  let resolver = getOrCreateResolver(event.params.node, event.address)
+  resolver.name = event.params.name
+  resolver.save()
+
   let resolverEvent = new NameChanged(createEventID(event))
   resolverEvent.resolver = createResolverID(event.params.node, event.address)
   resolverEvent.blockNumber = event.block.number.toI32()
